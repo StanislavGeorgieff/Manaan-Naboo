9ef7f2907fc26d2c6198ed2d77a21f3c42f3b55f
mihail-stefanov <mihail-stefanov@users.noreply.github.com>
2015-06-17 21:48:22 +0300

    Added two enum files.

---
+++ Manaan-Naboo/GameProject/GameProject/Enumerations/CollectableItemType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GameProject.Enumerations
+{
+    public enum CollectableItemType
+    {
+        Gold,
+        Arrows,
+        ManaPotion,
+        HealthPotion
+    }
+}
---
+++ Manaan-Naboo/GameProject/GameProject/Enumerations/ProjectileType.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GameProject.Enumerations
+{
+    public enum ProjectileType
+    {
+        Arrow,
+        Lighning
+    }
+}
